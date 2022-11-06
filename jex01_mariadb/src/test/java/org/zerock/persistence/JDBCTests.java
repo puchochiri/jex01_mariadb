6cf9676e1293819266e5c3287d07dc00140c015f
puchochiri <doawishfor@naver.com>
2022-11-06 22:50:00 +0900

    first commit

---
+++ jex01_mariadb/src/test/java/org/zerock/persistence/JDBCTests.java
@@ -0,0 +1,38 @@
+package org.zerock.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTests {
+	static {
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		
+		try(Connection con = 
+				DriverManager.getConnection(
+						"jdbc:mariadb://localhost:3306/01boardprj",
+						"root",
+						"1234")){
+			
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+	}
+
+}
