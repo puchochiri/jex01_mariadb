48be7bbf41307d586293061319a7745e427e3816
H2210756 <H2210756@HMC-YH-N041096.hkmc.hkmg.global>
2022-11-09 10:58:27 +0900

    20221109 commit

--- jex01_mariadb/src/test/java/org/zerock/controller/BoardControllerTests.java
+++ jex01_mariadb/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -31,13 +31,11 @@ public class BoardControllerTests {
 	
 	@Setter(onMethod_ = {@Autowired})
 	private WebApplicationContext ctx;
-	
 	private MockMvc mockMvc;
 	
 	@Before
 	public void setup() {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
-		
 	}
 	
 	//@Test
@@ -56,27 +54,23 @@ public class BoardControllerTests {
 	
 	@Test
 	public void testListPaging() throws Exception {
-		
 		log.info("testListPaging 실행");
 		log.info(mockMvc.perform(
 				MockMvcRequestBuilders.get("/board/list")
 				.param("pageNum", "2")
 				.param("amount", "50"))
 				.andReturn().getModelAndView().getModelMap());
-		
 	}
 	
 	
 	@Test
 	public void testRegister() throws Exception{
-		
 		log.info("testRegister 실행");
 		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/register")
 				.param("title", "테스트 새글 제목")
 				.param("content", "테스트 새글 내용")
 				.param("writer", "user00")
 			).andReturn().getModelAndView().getViewName();
-		
 		log.info(resultPage);
 	}
 	
@@ -89,7 +83,7 @@ public class BoardControllerTests {
 				.andReturn()
 				.getModelAndView().getModelMap());
 	}
-
+	
 	@Test
 	public void testModify() throws Exception {
 		log.info("testModify");
